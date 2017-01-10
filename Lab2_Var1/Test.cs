9792659c4362b97b5e723f2d748bfdf55f3a286e
rchurianov <rchurianov@gmail.com>
2017-01-10 16:20:12 +0300

    Added "Test" class
    Test class stores info about a term test: name and boolean field to tell with test was successfully passed or not.

---
+++ Lab2_Var1/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Var1
+{
+    public class Test
+    {
+        public Test(string name, bool passed)
+        {
+            this.Test_Name = name;
+            this.Test_Passed = passed;
+        }
+
+        public Test()
+        {
+            this.Test_Name = "Linear Algebra";
+            this.Test_Passed = true;
+        }
+
+        public string Test_Name
+        { get; set; }
+
+        public bool Test_Passed
+        { get; set; }
+
+        public override string ToString()
+        {
+            return Test_Name + " passed: " + Test_Passed.ToString();
+        }
+    }
+}
