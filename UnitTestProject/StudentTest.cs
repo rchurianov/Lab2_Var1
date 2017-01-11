97f6acb3bf6b9e9b9ae0f01b29719ab0f317f5e7
rchurianov <rchurianov@gmail.com>
2017-01-11 21:51:37 +0300

    Added new tests for Student class

---
+++ UnitTestProject/StudentTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab2_Var1;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void TestEquals()
+        {
+            Student s1 = new Student();
+            Student s2 = new Student();
+            Assert.AreNotEqual(null, s1);
+            Assert.AreEqual(false, s2.Equals(null));
+            Assert.AreEqual(s1, s2);
+            Assert.AreEqual(true, s2.Equals(s1));
+        }
+
+        [TestMethod]
+        public void GetHashCodeTest()
+        {
+            Student s1 = new Student();
+            Student s2 = new Student();
+            Assert.AreEqual(s1.GetHashCode(), s2.GetHashCode());
+        }
+
+    }
+}
