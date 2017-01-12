e60b03afe266658fcb5e5d1035582b5e6ab61f31
rchurianov <rchurianov@gmail.com>
2017-01-12 18:52:21 +0300

    StudentTest - added tests fr DeepCopy()

--- UnitTestProject/StudentTest.cs
+++ UnitTestProject/StudentTest.cs
@@ -26,5 +26,17 @@ namespace UnitTestProject
             Assert.AreEqual(s1.GetHashCode(), s2.GetHashCode());
         }
 
+        [TestMethod]
+        public void Test_DeepCopy()
+        {
+            IDateAndCopy s = new Student();
+            Student s_copy = (Student)s.DeepCopy();
+            Assert.AreEqual(s, s_copy);
+            s_copy.Credit_List.Add(new Credit());
+            s_copy.AddExams(new Exam());
+            Assert.AreNotEqual(true, s_copy.Equals(s));
+            Assert.AreNotEqual(s, s_copy);
+        }
+
     }
 }
